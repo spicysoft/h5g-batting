7dbaf58c073425e8a5903e9cab6a14ce761227ae
sekikawa shunsuke <>
2019-11-01 11:16:44 +0900

    アスペクト比の違いに対応 テクスチャの設定変更

---
+++ Assets/batting/Scripts/CameraSystem.cs
@@ -0,0 +1,81 @@
+using Unity.Entities;
+using Unity.Tiny.Core;
+using Unity.Tiny.Core2D;
+using Unity.Mathematics;
+using Unity.Tiny.Debugging;
+
+namespace Batting
+{
+	public class CameraSystem : ComponentSystem
+	{
+		EntityQuery TitleCameraQuery;
+		EntityQuery GameCameraQuery;
+
+		protected override void OnCreate()
+		{
+			EntityQueryDesc titleCameraDesc = new EntityQueryDesc()
+			{
+				All = new ComponentType[] { typeof( Camera2D ), typeof( CameraTitleTag ), typeof( CameraInfo ) }
+			};
+			TitleCameraQuery = GetEntityQuery( titleCameraDesc );
+
+
+			EntityQueryDesc gameCameraDesc = new EntityQueryDesc()
+			{
+				All = new ComponentType[] { typeof( Camera2D ), typeof( CameraInfo ) },
+				None = new ComponentType[] { typeof( CameraTitleTag ) }
+			};
+			GameCameraQuery = GetEntityQuery( gameCameraDesc );
+		}
+
+		protected override void OnUpdate()
+		{
+			Entities.With( GameCameraQuery ).ForEach( ( ref CameraInfo info, ref Camera2D camera ) => {
+				if( !info.Initialized ) {
+
+					// ディスプレイ情報.
+					var displayInfo = World.TinyEnvironment().GetConfigData<DisplayInfo>();
+					float frameW = displayInfo.frameWidth;
+					float frameH = (float)displayInfo.frameHeight;
+					float frameAsp = frameH / frameW;
+
+					// カメラ情報.
+					float rectW = camera.rect.width;
+					float rectH = camera.rect.height;
+					float rectAsp = rectH / rectW;
+
+					camera.halfVerticalSize = 1050f * frameAsp / rectAsp;
+
+					Debug.LogFormat( "----	game halfvert {0}", camera.halfVerticalSize );
+
+					info.Initialized = true;
+					return;
+				}
+			} );
+
+			Entities.With( TitleCameraQuery ).ForEach( ( ref CameraInfo info, ref Camera2D camera ) => {
+				if( !info.Initialized ) {
+
+					// ディスプレイ情報.
+					var displayInfo = World.TinyEnvironment().GetConfigData<DisplayInfo>();
+					float frameW = displayInfo.frameWidth;
+					float frameH = (float)displayInfo.frameHeight;
+					float frameAsp = frameH / frameW;
+
+					// カメラ情報.
+					float rectW = camera.rect.width;
+					float rectH = camera.rect.height;
+					float rectAsp = rectH / rectW;
+
+					camera.halfVerticalSize = (0.5f * rectH) * frameAsp / rectAsp;
+
+					Debug.LogFormat( "----	title halfvert {0}", camera.halfVerticalSize );
+
+					info.Initialized = true;
+					return;
+				}
+			} );
+
+		}
+	}
+}
